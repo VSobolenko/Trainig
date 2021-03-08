73aed2078766029b7bb27626057c7a40559e6cff
Vladislav <v.sobolenko.vs@gmail.com>
2021-03-09 01:03:06 +0300

    lab6 - very bed

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace External_training
             bool newSwitch = true;
 
             uint n = 9, m = 4;
-            int s = 0;
+            int s = 2;
 
             Console.WriteLine("Для одномерного массива, где N - его размер, введите N");
             VariableInitialization(ref n);
@@ -50,7 +50,7 @@ namespace External_training
                         break;
 
                     case 2:
-                        Console.WriteLine("\nДля передачи по ссылке возспользуемся методом, сортирующий массив до последнего чётного элемента\nВыполняется...");
+                        Console.WriteLine("\nДля передачи по ссылке возспользуемся методом, сортирующий массив до первого чётного элемента\nВыполняется...");
                         SortArrayWithRef(ref ex1);
                         Console.WriteLine("...Готово!\nОтсортированный массив по убыванию до последнего четного элемента: ");
                         PrintToConsoleArray(ref ex1);
@@ -65,7 +65,20 @@ namespace External_training
 
                         break;
                     case 4:
-                        Console.WriteLine("4");
+
+                        Console.WriteLine("Введите название посуды: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Введите матерьял посуды: ");
+                        string material = Console.ReadLine();
+                        Console.WriteLine("Введите цену: ");
+                        int price = Convert.ToInt32(Console.ReadLine());
+                        var dishes = Corteg(name, material, DateTime.Now, price);
+
+                        Console.WriteLine("Название: " + dishes.Item1 + "\n" +
+                            "Материал: : " + dishes.Item2 + "\n" +
+                            "Код товара: " + dishes.Item3 + "\n" +
+                            "Средняя цена: " + dishes.Item4 + "\n" +
+                            "Совпадение материала : " + dishes.Item5); 
                         break;
                     case 5:
                         newSwitch = false;
@@ -77,20 +90,79 @@ namespace External_training
 
             }
 
-            //Задание 2 - отсортировать по убыванию до последнего чётного элемента
-
-            //Задание 3 - номер последней строки с хотя бы 1 нечётным числом
-
             Console.ReadKey();
         }
 
+        static Tuple<string, string, string, double, string> Corteg(string name, string material, DateTime dateTime, int price)
+        {
+            Dishes[] dishes = new Dishes[5];
+            StructInitialization(ref dishes);
+            string codeDishes = material + "_" + dateTime.Day.ToString();
+            string YesOrNo = "No";
+            int totalPrice;
+            double averagePrice = 0;
+            int nom = -1;
+            for (int i = 0; i <= 2; i++)
+                if (dishes[i].name == name)
+                {
+                    nom = i;
+                    break;
+                }
+            if (nom != -1)
+            {
+                totalPrice = dishes[nom].price1 + dishes[nom].price2 + dishes[nom].price3;
+                averagePrice = totalPrice / 3;
+
+                if (dishes[nom].material == material)
+                {
+                    YesOrNo = "Yes";
+                }
+            }
+
+
+
+            return Tuple.Create<string, string, string, double, string>(name, material, codeDishes, averagePrice, YesOrNo);
+        }
+        private static void StructInitialization(ref Dishes[] dishes)
+        {
+            dishes[0].name = "Стакан";
+            dishes[1].name = "Бокал";
+            dishes[2].name = "Тарелка";
+            dishes[3].name = "Кувшин";
+            dishes[4].name = "Ложка";
+
+            dishes[0].material = "Пластик";
+            dishes[1].material = "Стекло";
+            dishes[2].material = "Металл";
+            dishes[3].material = "Фарфор";
+            dishes[4].material = "Дерево";
+
+            dishes[0].type = "Кухонная";
+            dishes[1].type = "Кухонная";
+            dishes[2].type = "Столовая";
+            dishes[3].type = "Столовая";
+            dishes[4].type = "Хранение";
+            Random rand = new Random();
+            int randomPrice;
+            for(int i =0; i < dishes.Length; i++)
+            {
+                randomPrice = rand.Next(50, 90);
+                dishes[i].price1 = randomPrice;
+
+                randomPrice = rand.Next(60, 80);
+                dishes[i].price2 = randomPrice;
+
+                randomPrice = rand.Next(55, 85);
+                dishes[i].price3 = randomPrice;
+            }
+        }
         private static void PrintMenu()
         {
             Console.WriteLine("\nМеню:");
             Console.Write("1 - передать данные по значению" + "\n" +
                 "2 - передать данные по ссылке" + "\n" +
-                "3 - использовать выходные параметры" + "\n" +
-                "4 - использовать параметры массивы" + "\n" +
+                "3 - использовать выходные параметры и параметры-массива" + "\n" +
+                "4 - задание со структурой" + "\n" +
                 "5 - выйти из программы" + "\n" +
                 "Ваш выбор: ");
         }
@@ -108,7 +180,12 @@ namespace External_training
         }
         private static void SortArrayWithRef(ref int[] arr)
         {
-            arr = arr.TakeWhile(x => x % 2 == 0).OrderByDescending(x => x).ToArray();
+            int[] sortArray = arr;
+            sortArray = sortArray.TakeWhile(x => x % 2 == 0).OrderByDescending(x => x).ToArray();
+            for (int i = 0; i < sortArray.Length; i++)
+            {
+                arr[i] = sortArray[i];
+            }
         }
         private static double SumOfElementArrayRef(ref int[,] ex1, ref int s)
         {
@@ -130,17 +207,6 @@ namespace External_training
                 }
             }
             return sumOfElements;
-        }
-        private static uint SelectionMenu(ref uint variable)
-        {
-            if (variable > 0 && variable < 6)
-            {
-                return variable;
-            }
-            else
-            {
-                throw new Exception();
-            }
         }
         static void VariableInitialization(ref uint size)
         {
@@ -195,7 +261,7 @@ namespace External_training
                     array[i] = constVar;
                     continue;
                 }
-                array[i] = rand.Next(0,100);
+                array[i] = rand.Next(0,99);
             }
         }
         static void PrintToConsoleArray(ref int[,] array)
@@ -204,7 +270,7 @@ namespace External_training
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write("{0,10:F7} ", array[i, j]);
+                    Console.Write("{0,3} ", array[i, j]);
                 }
                 Console.WriteLine();
             }
