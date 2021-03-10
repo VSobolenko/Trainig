574c650b0bd3c4b343c4b62907c6f151a630715c
Vladislav <v.sobolenko.vs@gmail.com>
2021-03-10 20:04:18 +0300

    test

--- External training/Program.cs
+++ External training/Program.cs
@@ -2,11 +2,20 @@
 
 namespace External_training
 {
-    class Program
+    public class Program
     {
+        public static double Average(params int[] variable)
+        {
+            int summ = 0;
+            foreach(var index in variable)
+            {
+                summ += index;
+            }
+            return summ / variable.Length;
+        }
         static void Main(string[] args)
         {
-
+            Console.WriteLine(Average(1,2,3,4));
             Console.ReadKey();
         }
     }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using External_training;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double r1 = Program.Average(1, 2, 3, 4);
+            Assert.AreEqual(2.5, r1);
+        }
+    }
+}
