ab87e7d6c424d65c30f0421f81a2295b26c25aed
Vladislav <v.sobolenko.vs@gmail.com>
2021-03-12 11:39:58 +0300

    completed unit-test

--- External training WPF/MainWindow.xaml.cs
+++ External training WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows;
@@ -6,58 +7,75 @@ using System.Windows.Documents;
 
 namespace External_training_WPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog newTextFile = new OpenFileDialog();
-
-            newTextFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            newTextFile.InitialDirectory = @"E:\Work\EPAM Training\test file\new text for read.txt";
-
-            if (newTextFile.ShowDialog() == true)
+            OpenFile();
+        }
+        public void OpenFile()
+        {
+            try
             {
-                TextRange doc = new TextRange(outputRichTextBox.Document.ContentStart, outputRichTextBox.Document.ContentEnd);
-                using (FileStream fs = new FileStream(newTextFile.FileName, FileMode.Open))
-                {
-                    if (Path.GetExtension(newTextFile.FileName).ToLower() == ".rtf")
-                        doc.Load(fs, DataFormats.Rtf);
-                    else if (Path.GetExtension(newTextFile.FileName).ToLower() == ".txt")
-                        doc.Load(fs, DataFormats.Text);
-                    else
-                        doc.Load(fs, DataFormats.Xaml);
-                }
-                doc.ApplyPropertyValue(Paragraph.MarginProperty, new Thickness(0));
-                doc.ApplyPropertyValue(Paragraph.FontSizeProperty, 20D);
-            }
+                OpenFileDialog newTextFile = new OpenFileDialog();
 
+                newTextFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                newTextFile.InitialDirectory = @"E:\Work\EPAM Training\test file\new text for read.txt";
+
+                if (newTextFile.ShowDialog() == true)
+                {
+                    TextRange doc = new TextRange(outputRichTextBox.Document.ContentStart, outputRichTextBox.Document.ContentEnd);
+                    using (FileStream fs = new FileStream(newTextFile.FileName, FileMode.Open))
+                    {
+                        if (Path.GetExtension(newTextFile.FileName).ToLower() == ".rtf")
+                            doc.Load(fs, DataFormats.Rtf);
+                        else if (Path.GetExtension(newTextFile.FileName).ToLower() == ".txt")
+                            doc.Load(fs, DataFormats.Text);
+                        else
+                            doc.Load(fs, DataFormats.Xaml);
+                    }
+                    doc.ApplyPropertyValue(Paragraph.MarginProperty, new Thickness(0));
+                    doc.ApplyPropertyValue(Paragraph.FontSizeProperty, 20D);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "Text Files (*.txt)|*.txt|RichText Files (*.rtf)|*.rtf|XAML Files (*.xaml)|*.xaml|All files (*.*)|*.*";
-            if (sfd.ShowDialog() == true)
+            SaveFile();
+        }
+        public void SaveFile()
+        {
+            try
             {
-                TextRange doc = new TextRange(outputRichTextBox.Document.ContentStart, outputRichTextBox.Document.ContentEnd);
-                using (FileStream fs = File.Create(sfd.FileName))
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Text Files (*.txt)|*.txt|RichText Files (*.rtf)|*.rtf|XAML Files (*.xaml)|*.xaml|All files (*.*)|*.*";
+                if (sfd.ShowDialog() == true)
                 {
-                    if (Path.GetExtension(sfd.FileName).ToLower() == ".rtf")
-                        doc.Save(fs, DataFormats.Rtf);
-                    else if (Path.GetExtension(sfd.FileName).ToLower() == ".txt")
-                        doc.Save(fs, DataFormats.Text);
-                    else
-                        doc.Save(fs, DataFormats.Xaml);
+                    TextRange doc = new TextRange(outputRichTextBox.Document.ContentStart, outputRichTextBox.Document.ContentEnd);
+                    using (FileStream fs = File.Create(sfd.FileName))
+                    {
+                        if (Path.GetExtension(sfd.FileName).ToLower() == ".rtf")
+                            doc.Save(fs, DataFormats.Rtf);
+                        else if (Path.GetExtension(sfd.FileName).ToLower() == ".txt")
+                            doc.Save(fs, DataFormats.Text);
+                        else
+                            doc.Save(fs, DataFormats.Xaml);
+                    }
                 }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
---
+++ UnitTestWPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestWPF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestWPF")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01e56175-23da-44de-988b-7ec77ab76be5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
